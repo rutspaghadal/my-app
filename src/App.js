e295144858efc96c1f1cb299ac5e25311790cb0e
Ritik <ritikpaghadal777@gmail.com>
2022-05-27 12:11:44 +0530

    27_5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ritik 1
         </a>
       </header>
     </div>
