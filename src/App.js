b8eb6ab0fe2c617cb1226beda14e8e49bd6548f1
Ritik <ritikpaghadal777@gmail.com>
2022-06-07 13:02:46 +0530

    7.1_6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Timefun from './Container/Time/Time';
+import Counter from './Container/Counter/Counter';
 
 function App(props) {
   return (
     <div>
       <Timefun />
+      <Counter />
     </div>
   );
 }
---
+++ src/Container/Counter/Counter.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+function Counter(props) {
+    let [counter, setCounter] = useState(0);
+
+    useEffect(() =>{
+        switch (counter) {
+            case counter + 1 <=10:
+
+                break;
+
+            default:
+                break;
+        }
+
+
+    },[])
+    return (
+        <div>
+            <h1>Counter :: {counter}</h1>
+
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={() => setCounter(counter - 1)}>-</button>
+            
+        </div>
+    );
+}
+
+export default Counter;
